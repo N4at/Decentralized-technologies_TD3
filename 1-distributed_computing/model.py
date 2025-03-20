d11a8ff33be0789c47e83785153e502ba47eb5d0
N4at <grandemange.nath@gmail.com>
2025-03-20 15:19:08 +0100

    Update model.py

--- 1-distributed_computing/model.py
+++ 1-distributed_computing/model.py
@@ -3,13 +3,10 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-# Charger le jeu de données Iris
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
 
-# Créer et entraîner un modèle KNeighborsClassifier
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
-# Sauvegarder le modèle
 joblib.dump(model, 'knn_model.pkl')
